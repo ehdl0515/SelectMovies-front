79dc1c0fe2b013a2a147a124b045dcec1b23c5c2
KDH <ehdl0515@naver.com>
2023-10-24 22:42:12 +0900

    [feat.new] user.js
    - input prompt 생성
    - 해당 정보 POST

---
+++ assets/js/user.js
@@ -0,0 +1,24 @@
+
+document.getElementById("post_user_btn1").addEventListener("click", postUser);
+
+function postUser() {
+
+    var userInputId = prompt("[1/2] 아이디를 입력하세요: ");
+    var userInputPw = prompt("[2/2] 암호를 입력하세요: ");
+
+
+    fetch("http://211.178.126.231:80/regist", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            // title: "Test",
+            // body: "I am testing!",
+            // userId: 1,
+            userId: userInputId,
+            userPw: userInputPw
+        }),
+    }).then((response) => console.log(response))
+        .then((data) => console.log(data));
+}
