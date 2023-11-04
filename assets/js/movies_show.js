769e8cf1f8ca53fef75a7472ee7e38ad8b438b32
KDH <ehdl0515@naver.com>
2023-11-04 11:30:03 +0900

    [feat.mod (js)] 영화목록 js 변경

--- assets/js/movies_show.js
+++ assets/js/movies_show.js
@@ -1,4 +1,4 @@
-
+/*
 // html 객체 선언
 let movie_title = document.getElementById("movie_title");
 
@@ -20,15 +20,69 @@ let movie_title = document.getElementById("movie_title");
 // 함수 정의
 async function GetMoviesOneById(movieCd) {
 
-    // 211.178.126.231
-    return fetch("http://127.0.0.1:8080/movies/one", {
+    return fetch("http://211.178.126.231/movies/one", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(movieCd),
         }
     )
+}
+
+let movie_cd = document.getElementById("movieCd");
+let movie_nm = document.getElementById("movieNm");
+let prdt_year = document.getElementById("prdtYear");
+let open_dt = document.getElementById("openDt");
+let type_nm = document.getElementById("typeNm");
+let prdt_stat_nm = document.getElementById("prdtStatNm");
+let rep_nation_nm = document.getElementById("repNationNm");
+let rep_genre_nm = document.getElementById("repGenreNm");
+*/
+
+let board_list_main = document.getElementById("board_list_main");
 
 
+
+// 로직 실행
+(async () => {
+    const response = await GetMoviesAll();
+    console.log("반환된 데이터:", response);
+    if (!response.ok) {
+        throw new Error('Network Connect Fail!!: ' + response.status);
+    }
+    let data = await response.json();
+    console.log(data);
+
+    let movies = data.content;
+    console.log(movies);
+
+    movies.map(function (movie, i) {
+        let new_html =  (
+            `<div id="movieCd" class="movieCd">${movie.movieCd}</div>
+                <div id="movieNm"  class="movieNm">${movie.movieNm}</div>
+                <div id="prdtYear" class="prdtYear">${movie.prdtYear}</div>
+                <div id="openDt" class="openDt">${movie.openDt}</div>
+                <div id="typeNm" class="typeNm">${movie.typeNm}</div>
+                <div id="prdtStatNm" class="prdtStatNm">${movie.prdtStatNm}</div>
+                <div id="repNationNm" class="repNationNm">${movie.repNationNm}</div>
+                <div id="repGenreNm" class="repGenreNm">${movie.repGenreNm}</div>`
+        );
+        container = document.createElement("div");
+        container.innerHTML = new_html;
+        board_list_main.appendChild(container);
+    });
+
+})();
+
+
+async function GetMoviesAll() {
+
+    // 211.178.126.231
+    return fetch("http://211.178.126.231/movies", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    )
 }
